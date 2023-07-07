d8a777585c1e17212fd8c7a45bef69e058b1f6fb
1Ankitk <110518334+1Ankitk@users.noreply.github.com>
2023-07-07 21:00:28 +0530

    mern_project

--- server.js
+++ server.js
@@ -15,14 +15,10 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
 
-// Serve Statis asset in production
-if (process.env.NODE_ENV === "production") {
-	// Set Static folder
-	app.use(express.static("client/build"));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
-}
+app.use(express.static(path.join(__dirname,"./client/build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
